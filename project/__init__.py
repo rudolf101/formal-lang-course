655bd854e6d3913c7fee62dcf7f889c32039d7f4
Vladislav Kasimov <vladislavkasimov@gmail.com>
2022-10-23 21:18:33 +0300

    Added CFG converter to WCNF and CFG file loader

--- project/__init__.py
+++ project/__init__.py
@@ -1,8 +1,10 @@
 import project.automata_utils
 import project.boolean_matrix
+import project.cfg_utils
 import project.graph_utils
 import project.rpq
 from project.automata_utils import *
 from project.boolean_matrix import *
+from project.cfg_utils import *
 from project.graph_utils import *
 from project.rpq import *
