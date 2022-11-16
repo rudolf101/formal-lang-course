9b378ac94a36454cd740f944a9173b1238d71b79
Vladislav Kasimov <vladislavkasimov@gmail.com>
2022-11-16 21:44:26 +0300

    Fixed init formatting

--- project/__init__.py
+++ project/__init__.py
@@ -1,6 +1,7 @@
 import project.automata_utils
 import project.boolean_matrix
 import project.cfg_utils
+import project.cfpq
 import project.cyk
 import project.ecfg
 import project.graph_utils
@@ -9,10 +10,9 @@ import project.rsm
 from project.automata_utils import *
 from project.boolean_matrix import *
 from project.cfg_utils import *
+from project.cfpq import *
 from project.cyk import *
 from project.ecfg import *
 from project.graph_utils import *
 from project.rpq import *
 from project.rsm import *
-import project.cfpq
-from project.cfpq import *
