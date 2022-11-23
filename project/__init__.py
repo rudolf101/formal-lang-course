a8806f61602ec06ebc9d3b85b7a1756efed23f23
Vladislav Kasimov <70969943+rudolf101@users.noreply.github.com>
2022-11-23 22:15:57 +0300

    Task 9 (#11)
    * Added Hellings algorithm with tests
    
    * Fixed init formatting

--- project/__init__.py
+++ project/__init__.py
@@ -1,6 +1,7 @@
 import project.automata_utils
 import project.boolean_matrix
 import project.cfg_utils
+import project.cfpq
 import project.cyk
 import project.ecfg
 import project.graph_utils
@@ -9,6 +10,7 @@ import project.rsm
 from project.automata_utils import *
 from project.boolean_matrix import *
 from project.cfg_utils import *
+from project.cfpq import *
 from project.cyk import *
 from project.ecfg import *
 from project.graph_utils import *
